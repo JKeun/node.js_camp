9845cf5b370968fe4d5a00cf1d4095980d656863
JKeun <jkeun1224@gmail.com>
2016-07-19 15:05:45 +0900

    init day2/: learn basic javascript & make chat application

---
+++ day2/app.js
@@ -0,0 +1,11 @@
+
+var http = require("http");
+
+var app = http.createServer(function(request, response) {
+    response.write("<h1>hello wolrd</h1>");
+    response.end();
+});
+
+app.listen(3000, function() {
+    console.log("Server is listening on localhost:3000");
+});
